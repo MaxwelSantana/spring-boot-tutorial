15c7514997222fc0ddce3d78165163d347d759cb
Maxwel <maxwelsant96@icloud.com>
2021-10-27 20:00:48 -0300

    Added student service and dependency injection

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,23 +11,10 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping
-    public List<Student> hello() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.AUGUST, 5),
-                        21
-                )
-        );
-    }
 }
