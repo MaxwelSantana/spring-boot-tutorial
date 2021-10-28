f588b499dc7ba4f6fc9caa1750517425acff3340
Maxwel <maxwelsant96@icloud.com>
2021-10-28 09:02:27 -0300

    Updated Student Put

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -17,14 +17,12 @@ public class StudentConfig {
             Student mariam = new Student(
                     "Mariam",
                     "mariam.jamal@gmail.com",
-                    LocalDate.of(2000, Month.AUGUST, 5),
-                    21
+                    LocalDate.of(2000, Month.AUGUST, 5)
             );
             Student alex = new Student(
                     "Alex",
                     "alex@gmail.com",
-                    LocalDate.of(2004, Month.AUGUST, 5),
-                    21
+                    LocalDate.of(2004, Month.AUGUST, 5)
             );
 
             repository.saveAll(List.of(mariam, alex));
