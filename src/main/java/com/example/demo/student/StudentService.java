163b827d66eec73427cdffcf35e307d799f109cc
Maxwel <maxwelsant96@icloud.com>
2021-10-28 07:56:48 -0300

    Added student repository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -9,15 +9,13 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.AUGUST, 5),
-                        21
-                )
-        );
+        return studentRepository.findAll();
     }
 }
